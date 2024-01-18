a75cbfba5eeba11f832a527eb94055abb204e918
Meric Arda Eren <aeren@netas.com.tr>
2024-01-18 11:29:33 +0300

    Refactor TodoItem and TodoList components

--- src/components/app/TodoItem/TodoItem.tsx
+++ src/components/app/TodoItem/TodoItem.tsx
@@ -1,15 +1,47 @@
-import { Todo } from '@prisma/client'
 import React from 'react'
 
-const TodoItem = (props: Todo) => {
+interface TodoItemProps {
+  name: string
+  description: string | null
+  createdAt: Date
+  updatedAt: Date
+  handleDeleteTodo: () => void
+}
+
+const TodoItem = (props: TodoItemProps) => {
+  const handleTodoItemDelete = () => {
+    props.handleDeleteTodo()
+  }
   return (
     <>
-      <div id="todo-item">
-        <div id="todo-item-name">{props.name}</div>
-        <div id="todo-item-description">{props.description}</div>
+      <div id="todo-item" className="">
+        <div id="todo-item-name" className="">
+          {props.name}
+        </div>
+        {props.description ? (
+          <div id="todo-item-description" className="">
+            {props.description}
+          </div>
+        ) : null}
+        <div id="todo-item-actions" className="">
+          <button id="todo-item-edit" className="">
+            Edit
+          </button>
+          <button
+            id="todo-item-delete"
+            className=""
+            onClick={handleTodoItemDelete}
+          >
+            Delete
+          </button>
+        </div>
         <div id="todo-time">
-          <div id="todo-item-created-at">{props.createdAt.toString()}</div>
-          <div id="todo-item-updated-at">{props.updatedAt.toString()}</div>
+          <div id="todo-item-created-at" className="">
+            {props.createdAt.toString()}
+          </div>
+          <div id="todo-item-updated-at" className="">
+            {props.updatedAt.toString()}
+          </div>
         </div>
       </div>
     </>
--- src/components/app/TodoList/TodoList.tsx
+++ src/components/app/TodoList/TodoList.tsx
@@ -1,5 +1,7 @@
 import { Todo } from '@prisma/client'
 import React from 'react'
+import { useDeleteTodo } from './hooks'
+import { TodoItem } from '..'
 
 interface TodoListProps {
   todos?: Todo[]
@@ -8,12 +10,27 @@ interface TodoListProps {
 const TodoList = (props: TodoListProps) => {
   const { todos } = props
   console.log('todos', todos)
+
+  const deleteTodo = useDeleteTodo()
+
+  const handleDeleteTodo = async (id: number) => {
+    await deleteTodo(id)
+  }
   return (
     <>
       <div id="todo-list">
-        <h1 className="text-4xl font-bold">Todo List</h1>
+        <h1>Todo List</h1>
         <ul className="list-disc">
-          {todos?.map((todo) => <li key={todo.id}>{todo.name}</li>)}
+          {todos?.map((todo) => (
+            <TodoItem
+              handleDeleteTodo={() => handleDeleteTodo(todo.id)}
+              key={todo.id}
+              description={todo.description}
+              name={todo.name}
+              createdAt={todo.createdAt}
+              updatedAt={todo.updatedAt}
+            />
+          ))}
         </ul>
       </div>
     </>
