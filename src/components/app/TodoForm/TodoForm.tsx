3e8199bb060f32d1448c8da84aad4ae36b7d3d8d
Meric Arda Eren <aeren@netas.com.tr>
2024-01-18 11:26:56 +0300

    Add classNames and useRouter imports to TodoForm component

--- src/components/app/TodoForm/TodoForm.tsx
+++ src/components/app/TodoForm/TodoForm.tsx
@@ -5,25 +5,42 @@ import { Button, Input } from '@/components'
 import { createTodoDto } from '@/dto'
 import { Todo } from '@prisma/client'
 import { useCreateTodo } from './hooks'
+import { classNames } from '@/utils/helpers'
+import { useRouter } from 'next/router'
 
 type createTodo = Omit<Todo, 'id' | 'createdAt' | 'updatedAt'>
 
 const TodoForm = () => {
   const methods = useForm<createTodo>({ resolver: zodResolver(createTodoDto) })
   const { handleSubmit, formState } = methods
-
+  const router = useRouter()
   const createUser = useCreateTodo()
 
   const onSubmit: SubmitHandler<
     Omit<Todo, 'id' | 'createdAt' | 'updatedAt'>
   > = async (data) => {
     await createUser(data)
+    if (formState.isSubmitSuccessful) {
+      methods.reset()
+    }
+  }
+
+  if (formState.isSubmitSuccessful) {
+    router.push('/')
+    return <h1>Success Redirecting to homepage...</h1>
   }
 
   return (
     <>
       <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={classNames(
+            'flex flex-col gap-2 rounded-md p-8',
+            'border border-gray-200 bg-white shadow-md'
+          )}
+        >
+          <h1 className="font-semibold text-2xl">Add Todo</h1>
           <Input label="Name" name="name" placeholder="Please enter a name." />
           <Input
             label="Description"
