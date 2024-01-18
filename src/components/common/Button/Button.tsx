ef69bc43bd90700257d467308573bc79ae216169
Meric Arda Eren <aeren@netas.com.tr>
2024-01-18 11:25:30 +0300

    Update Button and Input components

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -16,8 +16,8 @@ const Button = (props: ButtonProps) => {
         onClick={onclick}
         disabled={disabled}
         className={classNames(
-          'bg-blue-700 transition ease-in-out duration-700 disabled:bg-gray-500 disabled:cursor-not-allowed hover:bg-[#2e7b80] text-white font-bold focus:outline-none flex place-content-center',
-          'mt-2 h-10 self-end w-1/4 py-2 px-4 rounded'
+          'bg-blue-700 transition ease-in-out duration-700 disabled:bg-gray-500 disabled:cursor-not-allowed hover:bg-[#2e7b80] text-white font-medium text-md focus:outline-none flex place-content-center',
+          'mt-2 h-10 self-end w-1/6 py-2 px-4 rounded'
         )}
         type={type}
       >
