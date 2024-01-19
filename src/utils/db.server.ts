90b9b1310ad8923963d3682e058d886c8a40ade9
Meric Arda Eren <aeren@netas.com.tr>
2024-01-19 21:14:38 +0300

    Refactor database connection in db.server.ts

--- src/utils/db.server.ts
+++ src/utils/db.server.ts
@@ -1,13 +1,22 @@
+/* eslint no-var: 0 */
 import { PrismaClient, Prisma } from '@prisma/client'
 
-export class DatabaseManager {
-  public static PrismaSingleton: PrismaClient
-
-  static getPrisma() {
-    this.PrismaSingleton ||= new PrismaClient()
-    return this.PrismaSingleton
-  }
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-export const db = DatabaseManager.getPrisma()
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+if (!global.prisma) {
+  global.prisma = prismaClientSingleton()
+}
+
+export const db = global.prisma
+
 export { Prisma }
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = global.prisma
+}
