936ba72428676d6290b71ed6527756355f0a8e1f
Meric Arda Eren <aeren@netas.com.tr>
2024-01-19 21:15:08 +0300

    Refactor loading component

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import LoadingSVG from './Loading.svg'
 
-// TODO: Test loading component
 const Loading = () => {
   return (
     <>
-      <div
-        id="loading"
-        className="flex flex-col items-center justify-center h-20 bg-gray-100"
-      >
-        <div className="animate-spin">
-          <LoadingSVG className="amimate-spin fill-blue-700" />
+      <div className="flex h-screen grow flex-col items-center justify-center">
+        <div
+          id="loading"
+          className="flex flex-col items-center justify-center h-20"
+        >
+          <div className="animate-spin">
+            <LoadingSVG className="amimate-spin fill-blue-700" />
+          </div>
+          <div>Loading...</div>
         </div>
-        <div>Loading...</div>
       </div>
     </>
   )
