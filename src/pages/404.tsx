8f2034bf0074bc3559c024de98009e70873c8630
Meric Arda Eren <aeren@netas.com.tr>
2024-01-19 20:14:55 +0300

    Implement better ui for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,10 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <>
+      <div className="flex h-screen grow flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-md">Page Not Found</p>
+      </div>
+    </>
+  )
 }
