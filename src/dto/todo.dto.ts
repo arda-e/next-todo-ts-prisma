0c2acfeead7488a29685f912234403141e1b763f
Meric Arda Eren <aeren@netas.com.tr>
2024-01-19 20:26:05 +0300

    Add documentation for dtos and types

--- src/dto/todo.dto.ts
+++ src/dto/todo.dto.ts
@@ -1,20 +1,47 @@
 import { z } from 'zod'
 
+/**
+ * Represents the data transfer object (DTO) for creating a new todo.
+ *
+ * @typedef {Object} CreateTodoDto
+ * @property {string} name - The name of the todo (required).
+ * @property {string} [description] - The description of the todo (optional).
+ */
 export const createTodoDto = z.object({
   name: z.string().min(1, 'Name is required'),
   description: z.string().optional(),
 })
 
+/**
+ * Represents the data transfer object (DTO) for updating an existing todo.
+ *
+ * @typedef {Object} UpdateTodoDto
+ * @property {number} id - The ID of the todo to update (positive integer).
+ * @property {string} [name] - The updated name of the todo (optional).
+ * @property {string} [description] - The updated description of the todo (optional).
+ */
 export const updateTodoDto = z.object({
   id: z.coerce.number().int().positive(),
   name: z.string().min(1, 'Name is required').optional(),
   description: z.string().optional(),
 })
 
+/**
+ * Represents the data transfer object (DTO) for deleting a todo.
+ *
+ * @typedef {Object} DeleteTodoDto
+ * @property {number} id - The ID of the todo to delete (positive integer).
+ */
 export const deleteTodoDto = z.object({
   id: z.coerce.number().int().positive(),
 })
 
+/**
+ * Represents the data transfer object (DTO) for finding a todo by its ID.
+ *
+ * @typedef {Object} FindTodoDto
+ * @property {number} id - The ID of the todo to find (positive integer).
+ */
 export const findTodoDto = z.object({
   id: z.coerce.number().int().positive(),
 })
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,9 @@
+/**
+ * Represents a response object that typically includes data, a message, and a success indicator.
+ *
+ * @template T - The type of data contained in the response.
+ * @interface GetResponse
+ */
 export interface GetResponse<T> {
   data: T
   message: string
